e6f881b65a8914059a1f294ca2580674066ba7cf
ShehabRagaai <125378546+ShehabRagaai@users.noreply.github.com>
2023-02-14 23:09:16 +0100

    Uplaoding Ultrasonic Sensor Drivers

---
+++ HAL/ULTRASONIC_Sensor/ULTRASONIC_interface.h
@@ -0,0 +1,14 @@
+/*
+ * ULTRASONIC_program.c
+ *
+ *  Created on: 22 Dec 2022
+ *      Author: sheha
+ */
+
+#ifndef ULTRASONIC_PROGRAM_C_
+#define ULTRASONIC_PROGRAM_C_
+
+void UltraSonic_voidInit(void);
+u8 UltraSonic_u8TrigerAndDistanceRead(u16 *Copy_u8Distance);
+
+#endif /* ULTRASONIC_PROGRAM_C_ */
---
+++ HAL/ULTRASONIC_Sensor/ULTRASONIC_private.h
@@ -0,0 +1,9 @@
+
+
+#ifndef ULTRASONIC_PRIVATE_H_
+#define ULTRASONIC_PRIVATE_H_
+
+#define IDEL 1
+#define BUSY 0
+
+#endif /* ULTRASONIC_PRIVATE_H_ */
